75b0100648aadcb1801a05bfd9f2af1d35edcb0a
Jure Vrscaj <jure.vrscaj@gmail.com>
2018-01-19 19:01:40 +0100

    Show mnemonic info

--- privatekey_to_info.py
+++ privatekey_to_info.py
@@ -1,10 +1,31 @@
 import logging
 from pprint import pprint
-
+import textwrap
+import json
+import sys
 from bitcash import Key
 
+from checksum import checksum
+from int_to_bitstr import int_to_bitstr
+
+words_rev = json.loads(open("english_reverse.json").read())
+
+def bitstr_to_mnemonic(bitstr):
+    assert(len(bitstr) == 256), "Bad bitstr size: %s" % len(bitstr)
+    mnemonic = []
+    for bits in textwrap.wrap(bitstr, 11):
+        if len(bits) != 11:
+            bits += int_to_bitstr(checksum(bitstr))
+        mnemonic.append(words_rev[bits])
+    return mnemonic
+
 def main(privatekey):
-    bytes = (int(privatekey, 16)).to_bytes(32, byteorder='big')    
+    if len(privatekey) == 256:
+        bytes = (int(privatekey, 2)).to_bytes(32, byteorder='big')    
+    elif len(privatekey) == 64:
+        bytes = (int(privatekey, 16)).to_bytes(32, byteorder='big')    
+    else:
+        raise(Exception('wrong privatekey length: %s' % len(privatekey)))
     k = None
     balance = None
     try:
@@ -17,8 +38,9 @@ def main(privatekey):
         'key': k,
         'address': k and k.address,
         'balance': balance,
-        'wif': k and k.to_wif()
+        'wif': k and k.to_wif(),
+        'mnemonic': " ".join(bitstr_to_mnemonic("".join(bin(x)[2:].zfill(8) for x in bytes)))
     }
     
 if __name__ == '__main__':
-    pprint(main(sys.argv[1:]))
+    pprint(main(sys.argv[1]))
